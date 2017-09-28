af042d43e0241338682aee9bd239f8df244b8368
DUHUN HWANG <yelobean@DUHUNui-MacBook-Pro.local>
2017-09-28 20:19:30 +0900

    Initial Commit

---
+++ Ordinary Camera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ordinary Camera
+//
+//  Created by DUHUN HWANG on 2017. 9. 28..
+//  Copyright © 2017년 YELOBEAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
