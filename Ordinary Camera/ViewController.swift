14387d74b5a604dd7188d512ce400accbb82f9d6
Jun Seo <tgw4460@naver.com>
2017-10-22 17:58:03 +0900

    For filtering the view, I used the queue and it was chainging previewView UIimageView(Originally it is the preview, but I changed to UIimageView)

--- Ordinary Camera/ViewController.swift
+++ Ordinary Camera/ViewController.swift
@@ -9,23 +9,28 @@
 import UIKit
 import AVFoundation
 import Photos
+import CoreMedia // Jun
+import CoreImage
+var previewLayer : AVCaptureVideoPreviewLayer?
 
 enum LiveMode {
     
     case on, off, unavailable
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate { //Jun
     
+    let mainGroup = UIStackView()
+    var filteredImage: UIImage?
     // intializing class variable
     var captureSession = AVCaptureSession()
-    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
     var input: AVCaptureDeviceInput?
     var capturePhotoOutput = AVCapturePhotoOutput()
     var imageData: Data?
     var timer = Timer()
     var timerCount = 0
     // \initializing class variable
+
     
     // initializing option variable
     var flashMode = AVCaptureDevice.FlashMode.off
@@ -35,7 +40,7 @@ class ViewController: UIViewController {
     // \initializing option variable
     
     // IB OUTLET
-    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var previewView: UIImageView!
     @IBOutlet weak var timerButton: UIButton!
     @IBOutlet weak var flashButton: UIButton!
     @IBOutlet weak var liveButton: UIButton!
@@ -52,10 +57,7 @@ class ViewController: UIViewController {
         
         
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.processTimer), userInfo: nil, repeats: true)
- 
-        
     }
-    
     
     // OPTION CONTROL
     @IBAction func camPositionAction(_ sender: UIButton) {
@@ -66,15 +68,12 @@ class ViewController: UIViewController {
             cameraPosition = AVCaptureDevice.Position.front
         }
         
-        
-        
         captureSession.removeInput(captureSession.inputs.first as AVCaptureInput!)
         captureSession.removeOutput(captureSession.outputs.first as AVCaptureOutput!)
         
         captureSession.stopRunning()
         loadCamera()
     }
-    
     
     @IBAction func timerAction(_ sender: UIButton) {
         
@@ -117,8 +116,6 @@ class ViewController: UIViewController {
     // \OPTION CONTROL
     // \IB ACTION
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -131,8 +128,6 @@ class ViewController: UIViewController {
         // \load setting (user defaults)
         
         loadCamera()
-        
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -140,13 +135,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
-    
-    // basic function for camera operation
+    override func viewDidLayoutSubviews()
+    {
+        let topMargin = topLayoutGuide.length
+        
+        mainGroup.frame = CGRect(x: 0, y: topMargin, width: view.frame.width, height: view.frame.height - topMargin).insetBy(dx: 5, dy: 5)
+    }
     
     func loadCamera(){
         let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: AVMediaType.video, position: cameraPosition) // how to selfie camera
-        
         
         do {
             input = try AVCaptureDeviceInput(device: captureDevice!)
@@ -154,14 +151,24 @@ class ViewController: UIViewController {
             captureSession.addInput(input!)
             
             captureSession.addOutput(capturePhotoOutput)
+
+            let videoOutput = AVCaptureVideoDataOutput()
+            videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue.main)
+            videoOutput.alwaysDiscardsLateVideoFrames = true
+            if captureSession.canAddOutput(videoOutput) {
+                captureSession.addOutput(videoOutput)
+            }
             
-            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspect
+            //Below sentence invoked captureOutput.
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            previewLayer!.videoGravity = AVLayerVideoGravity.resizeAspect
             
-            videoPreviewLayer?.frame = previewView.layer.bounds
-            previewView.layer.addSublayer(videoPreviewLayer!)
+//            previewLayer!.frame = previewView.layer.bounds
+            previewView.layer.addSublayer(previewLayer!)
+            print("previewLayer")
+            print(previewLayer)
             
-            captureSession.sessionPreset = .photo
+            captureSession.sessionPreset = AVCaptureSession.Preset.photo
             
             if !capturePhotoOutput.isLivePhotoCaptureSupported {
                 liveMode = .unavailable
@@ -172,7 +179,7 @@ class ViewController: UIViewController {
                 capturePhotoOutput.isLivePhotoCaptureEnabled = capturePhotoOutput.isLivePhotoCaptureSupported
             }
             capturePhotoOutput.isHighResolutionCaptureEnabled = true
-            
+
             captureSession.startRunning()
             
         } catch {
@@ -180,49 +187,61 @@ class ViewController: UIViewController {
             print(error)
         }
     }
-    
+
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        let ComicEffectFilter = CIFilter(name: "CIComicEffect")
+        guard let filter = ComicEffectFilter else{
+            return
+        }
+        
+        let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
+        let cameraImage = CIImage(cvPixelBuffer: pixelBuffer!)
+        
+        filter.setValue(cameraImage, forKey: kCIInputImageKey)
+        
+        filteredImage = UIImage(ciImage: filter.value(forKey: kCIOutputImageKey) as! CIImage!)
+        print(filteredImage)
+        DispatchQueue.main.async {
+            self.previewView.image = self.filteredImage
+        }
+    }
 }
 
-
-
-
-
-
-
-
-
-
 // other functions about delegate
-
 extension ViewController : AVCapturePhotoCaptureDelegate {
     
     // delegate
     func photoOutput(_: AVCapturePhotoOutput, didFinishProcessingPhoto: AVCapturePhoto, error: Error?) {
         
+        print("photo1")
         self.imageData = didFinishProcessingPhoto.fileDataRepresentation()
         
         
         if !capturePhotoOutput.isLivePhotoCaptureEnabled {
+//            let capturedImage = UIImage.init(data: imageData! , scale: 1.0)
+//            if let image = capturedImage {
+//                // Save our captured image to photos album
+//                UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+//            }
             let capturedImage = UIImage.init(data: imageData! , scale: 1.0)
-            if let image = capturedImage {
+            if let image = self.previewView.image {
                 // Save our captured image to photos album
+                print("photo1 image")
                 UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
             }
+//            UIImageWriteToSavedPhotosAlbum(self.previewView.image, nil, nil)
+//            UIImageWriteToSavedPhotosAlbum(self.filteredImage!, nil, nil, nil)
             
         }
-        
         cameraButton.isEnabled = true
     }
     
-    
-    
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingLivePhotoToMovieFileAt outputFileURL: URL, duration: CMTime, photoDisplayTime: CMTime, resolvedSettings: AVCaptureResolvedPhotoSettings, error: Error?){
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingLivePhotoToMovieFileAt outputFileURL: URL, duration: CMTime, photoDisplayTime: CMTime, resolvedSettings: AVCaptureResolvedPhotoSettings, error: Error?) {
         
-        
+        print("photo 2")
         PHPhotoLibrary.shared().performChanges({ [unowned self] in
             let creationRequest = PHAssetCreationRequest.forAsset()
             creationRequest.addResource(with: .photo, data: self.imageData!, options: nil)
-            
             
             let options = PHAssetResourceCreationOptions()
             options.shouldMoveFile = true
@@ -234,7 +253,6 @@ extension ViewController : AVCapturePhotoCaptureDelegate {
                     print("Error occurered while saving photo to photo library: \(error)")
                 }
                 
-                
                 if FileManager.default.fileExists(atPath: outputFileURL.path) {
                     do {
                         try FileManager.default.removeItem(atPath: outputFileURL.path)
@@ -243,8 +261,8 @@ extension ViewController : AVCapturePhotoCaptureDelegate {
                         print("Could not remove file at url: \(outputFileURL.path)")
                     }
                 } // change to func? if not exist?
-                
-        })
+            }
+        )
     }
     // \delegate
     
@@ -275,9 +293,7 @@ extension ViewController : AVCapturePhotoCaptureDelegate {
         }
         timerCount -= 1
     }
-    
     // \selector
-    
 }
 
 
